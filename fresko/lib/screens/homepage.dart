4d0fa18628545e1c964bf2bbc356a9354203bb22
pratikjain04 <pratikjain0411@gmail.com>
2018-10-07 02:45:15 +0530

    Drawer Added
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

---
+++ fresko/lib/screens/homepage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../Generics/Colors.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    
+    double uni_height = MediaQuery.of(context).size.height;
+    double uni_width = MediaQuery.of(context).size.width;
+    
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Color(0xff16ad32),
+        title: new Text('Freshko'),
+      ),
+      drawer: new Drawer(
+
+        child: new Container(
+          color: Colors.white,
+            padding: EdgeInsets.only(top: 32.0, left: 55.0, right: 55.0),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                   Padding(
+                     padding: EdgeInsets.only(left: uni_width/25),
+                     child: Container(
+                       height: uni_height/7,
+                       width: uni_width/4,
+                       child: Image(
+                           image: AssetImage('assets/images/FreshkoLogo.png'),
+                       ),
+                     ),
+                   ),
+              Container(
+                  height: uni_height/7,
+                  width: uni_width/2,
+                  child: Image(
+                      image: AssetImage('assets/images/FreshkoName.jpeg'))
+              ),
+              ],
+            )
+        ),
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            children: <Widget>[new Text('No Widgets here for the moment')],
+          ),
+        ),
+      ),
+    );
+  }
+}
