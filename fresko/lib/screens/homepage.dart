f4890c7ecc6082a5f30764462099cea1d3398c00
pratikjain04 <pratikjain0411@gmail.com>
2018-10-07 13:51:31 +0530

    Drawer Updates
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- fresko/lib/screens/homepage.dart
+++ fresko/lib/screens/homepage.dart
@@ -16,47 +16,62 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    
     double uni_height = MediaQuery.of(context).size.height;
     double uni_width = MediaQuery.of(context).size.width;
-    
+
     return new Scaffold(
       appBar: new AppBar(
         backgroundColor: Color(0xff16ad32),
         title: new Text('Freshko'),
       ),
       drawer: new Drawer(
-
         child: new Container(
-          color: Colors.white,
+            color: Colors.white,
             padding: EdgeInsets.only(top: 32.0, left: 55.0, right: 55.0),
             child: new Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                   Padding(
-                     padding: EdgeInsets.only(left: uni_width/25),
-                     child: Container(
-                       height: uni_height/7,
-                       width: uni_width/4,
-                       child: Image(
-                           image: AssetImage('assets/images/FreshkoLogo.png'),
-                       ),
-                     ),
-                   ),
-              Container(
-                  height: uni_height/7,
-                  width: uni_width/2,
-                  child: Image(
-                      image: AssetImage('assets/images/FreshkoName.jpeg'))
-              ),
+                Padding(
+                  padding: EdgeInsets.only(left: uni_width / 25),
+                  child: Container(
+                    height: uni_height / 7,
+                    width: uni_width / 4,
+                    child: Image(
+                      image: AssetImage('assets/images/FreshkoLogo.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                    height: uni_height / 7,
+                    width: uni_width / 2,
+                    child: Image(
+                        image: AssetImage('assets/images/CompanyName.jpeg')
+                    )),
+                Container(
+                  height: 1.2,
+                  color: Colors.black87,
+                ),
+                ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
+                      FlatButton(onPressed: (){}, child: Text('Your Profile')),
+                      FlatButton(onPressed: (){}, child: Text('Your Orders')),
+                      FlatButton(onPressed: (){}, child: Text('Categories'),)
+                  ],
+
+                )
               ],
-            )
-        ),
+
+            )),
+
+
       ),
       body: new Container(
         child: new Center(
           child: new Column(
-            children: <Widget>[new Text('No Widgets here for the moment')],
+            children: <Widget>[
+              new Text(''),
+            ],
           ),
         ),
       ),
