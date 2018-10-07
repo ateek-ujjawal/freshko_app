6c337e18d83704615d884c877104b993d42cf636
ateek-ujjawal <ateekujjawal@gmail.com>
2018-10-07 14:29:45 +0530

    Test commits
    Signed-off-by: ateek-ujjawal <ateekujjawal@gmail.com>

--- fresko/lib/screens/homepage.dart
+++ fresko/lib/screens/homepage.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import '../Generics/Colors.dart';
+import 'test.dart';
+import 'SlideRight.dart';
 
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
     home: new HomePage(),
+    routes: <String, WidgetBuilder>{
+      '/Profile': (BuildContext context) => Profile()
+    },
   ));
 }
 
@@ -24,48 +29,7 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Color(0xff16ad32),
         title: new Text('Freshko'),
       ),
-      drawer: new Drawer(
-        child: new Container(
-            color: Colors.white,
-            padding: EdgeInsets.only(top: 32.0, left: 55.0, right: 55.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: uni_width / 25),
-                  child: Container(
-                    height: uni_height / 7,
-                    width: uni_width / 4,
-                    child: Image(
-                      image: AssetImage('assets/images/FreshkoLogo.png'),
-                    ),
-                  ),
-                ),
-                Container(
-                    height: uni_height / 7,
-                    width: uni_width / 2,
-                    child: Image(
-                        image: AssetImage('assets/images/CompanyName.jpeg')
-                    )),
-                Container(
-                  height: 1.2,
-                  color: Colors.black87,
-                ),
-                ListView(
-                  shrinkWrap: true,
-                  children: <Widget>[
-                      FlatButton(onPressed: (){}, child: Text('Your Profile')),
-                      FlatButton(onPressed: (){}, child: Text('Your Orders')),
-                      FlatButton(onPressed: (){}, child: Text('Categories'),)
-                  ],
-
-                )
-              ],
-
-            )),
-
-
-      ),
+      drawer: CustomDrawer(uni_width: uni_width, uni_height: uni_height),
       body: new Container(
         child: new Center(
           child: new Column(
@@ -77,4 +41,56 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+class CustomDrawer extends StatelessWidget {
+
+  final double uni_width, uni_height;
+  CustomDrawer({this.uni_width, this.uni_height});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: new Container(
+          color: Colors.white,
+          padding: EdgeInsets.only(top: 32.0, left: 55.0, right: 55.0),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: uni_width / 25),
+                child: Container(
+                  height: uni_height / 7,
+                  width: uni_width / 4,
+                  child: Image(
+                    image: AssetImage('assets/images/FreshkoLogo.png'),
+                  ),
+                ),
+              ),
+              Container(
+                  height: uni_height / 7,
+                  width: uni_width / 2,
+                  child: Image(
+                      image: AssetImage('assets/images/CompanyName.jpeg')
+                  )),
+              Container(
+                height: 1.2,
+                color: Colors.black87,
+              ),
+              ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: (){Navigator.of(context).push(SlideRightRoute(widget: Profile()));},
+                    child: Text('Your Profile')
+                  ),
+                  FlatButton(onPressed: (){}, child: Text('Your Orders')),
+                  FlatButton(onPressed: (){}, child: Text('Categories'))
+                ],
+              )
+            ],
+          )),
+    );
+  }
+
 }
