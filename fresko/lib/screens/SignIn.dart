f45e048f4b9b1c7694fd67bca9912dd21835b61e
pratikjain04 <pratikjain0411@gmail.com>
2018-10-07 16:15:28 +0530

    Login Template Beta2 Added
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

--- fresko/lib/screens/SignIn.dart
+++ fresko/lib/screens/SignIn.dart
@@ -21,7 +21,7 @@ class _SignInState extends State<SignIn> {
           fit: BoxFit.cover,
         ),
         Padding(
-          padding: EdgeInsets.all(uni_height/13),
+          padding: EdgeInsets.all(uni_height / 13),
           child: Card(
             color: Colors.transparent,
             shape: RoundedRectangleBorder(
@@ -42,7 +42,13 @@ class _SignInState extends State<SignIn> {
                         ),
                       ),
                       Padding(padding: EdgeInsets.only(top: 20.0)),
-                      Text('Sign In', style: TextStyle(fontSize: 25.0, color: Colors.white, fontWeight: FontWeight.bold),)
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      )
                       //todo: Change fontFamily
                     ],
                   ),
@@ -57,12 +63,64 @@ class _SignInState extends State<SignIn> {
                 Expanded(
                   child: Container(
                     height: 400.0,
+                    width: 400.0,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10.0),
-                        bottomRight: Radius.circular(10.0),
-                      ),
-                      color: Color(0xFF3C4D5D).withOpacity(0.75)
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                        color: Color(0xFF3C4D5D).withOpacity(0.75)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        //USERNAME FIELD
+                        Container(
+                          height: 50.0,
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30.0),
+                            color: Colors.white,
+                          ),
+                          child: Center(
+                              child: Text(
+                            'Username',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15.0),
+                          )),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 15.0)),
+                        //PASSWORD FIELD
+                        Container(
+                          height: 50.0,
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30.0),
+                            color: Colors.white,
+                          ),
+                          child: Center(
+                              child: Text(
+                            'Password',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15.0),
+                          )),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 50.0)),
+                        //SIGN IN BUTTON
+                        Container(
+                          height: 50.0,
+                          width: 200.0,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0)),
+                            onPressed: () {},
+                            color: themeColor,
+                            child: Text(
+                              'SIGN IN',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 )
